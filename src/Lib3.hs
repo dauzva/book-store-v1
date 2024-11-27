57d5b06ea0e189da09f0483052b6b855ca3f94bd
Darijus Daužvardis <dauzva@gmail.com>
2024-11-27 02:24:11 +0000

    added lib3

--- src/Lib3.hs
+++ src/Lib3.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE InstanceSigs #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module Lib3
     ( stateTransition,
     StorageOp (..),
@@ -6,12 +7,19 @@ module Lib3
     parseCommand,
     parseStatements,
     marshallState,
-    renderStatements
+    renderStatements,
+    Statements(..),
+    Command(..)
     ) where
 
-import Control.Concurrent ( Chan )
-import Control.Concurrent.STM(STM, TVar)
+import Control.Concurrent ( Chan, writeChan, readChan )
+import Control.Concurrent.STM( STM, TVar, atomically, readTVar, writeTVar, readTVarIO )
+import Control.Monad ( when )
+import System.IO ( withFile, IOMode(ReadMode, WriteMode), hPutStrLn, hGetContents )
 import qualified Lib2
+import Control.Concurrent.Chan (newChan)
+import GHC.IO.Handle
+import GHC.IO.Handle.FD
 
 data StorageOp = Save String (Chan ()) | Load (Chan String)
 -- | This function is started from main
@@ -21,12 +29,23 @@ data StorageOp = Save String (Chan ()) | Load (Chan String)
 -- to a channel provided in a request.
 -- Modify as needed.
 storageOpLoop :: Chan StorageOp -> IO ()
-storageOpLoop _ = do
-  return $ error "Not implemented 1"
+storageOpLoop ioChan = do
+    op <- readChan ioChan
+    case op of
+        Save content responseChan -> do
+            handle <- openFile "state.txt" WriteMode
+            hPutStrLn handle content
+            writeChan responseChan ()
+            hClose handle
+        Load responseChan -> do
+            handle <- openFile "state.txt" ReadMode
+            content <- hGetContents handle
+            writeChan responseChan content
+    storageOpLoop ioChan
 
 data Statements = Batch [Lib2.Query] |
-               Single Lib2.Query
-               deriving (Show, Eq)
+                 Single Lib2.Query
+                 deriving (Show, Eq)
 
 data Command = StatementCommand Statements |
                LoadCommand |
@@ -35,19 +54,37 @@ data Command = StatementCommand Statements |
 
 -- | Parses user's input.
 parseCommand :: String -> Either String (Command, String)
-parseCommand _ = Left "Not implemented 2"
+parseCommand str
+    | str == "load" = Right (LoadCommand, "")
+    | str == "save" = Right (SaveCommand, "")
+    | otherwise = case parseStatements str of
+        Left err -> Left err
+        Right (statements, rest) -> Right (StatementCommand statements, rest)
 
 -- | Parses Statement.
 -- Must be used in parseCommand.
 -- Reuse Lib2 as much as you can.
 -- You can change Lib2.parseQuery signature if needed.
 parseStatements :: String -> Either String (Statements, String)
-parseStatements _ = Left "Not implemented 3"
+parseStatements str =
+    let trimmed = dropWhile (== ' ') str
+        linesOfInput = lines trimmed
+    in parseQueries linesOfInput []
+
+  where
+    parseQueries :: [String] -> [Lib2.Query] -> Either String (Statements, String)
+    parseQueries [] acc = Right (Batch (reverse acc), "")
+    parseQueries (line:rest) acc
+        | null (dropWhile (== ' ') line) = parseQueries rest acc -- Skip empty lines
+        | otherwise =
+            case Lib2.parseQuery line of
+                Left err -> Left $ "Error parsing query: " ++ err
+                Right query -> parseQueries rest (query : acc)
 
 -- | Converts program's state into Statements
 -- (probably a batch, but might be a single query)
 marshallState :: Lib2.State -> Statements
-marshallState _ = error "Not implemented 4"
+marshallState (Lib2.State queries) = Batch queries
 
 -- | Renders Statements into a String which
 -- can be parsed back into Statements by parseStatements
@@ -56,7 +93,20 @@ marshallState _ = error "Not implemented 4"
 -- Must have a property test
 -- for all s: parseStatements (renderStatements s) == Right(s, "")
 renderStatements :: Statements -> String
-renderStatements _ = error "Not implemented 5"
+renderStatements (Single query) = renderStatements (Batch [query])
+renderStatements (Batch queries) = unlines $ map getStringFromQuery queries
+
+getStringFromQuery :: Lib2.Query -> String
+getStringFromQuery q = do
+  case q of
+    Lib2.AddQuery a ->
+        let
+            dropid = drop 3 a
+            title = takeWhile (/= ',') dropid
+            rest = drop (length title) dropid
+        in
+            "add " ++ "\"" ++ title ++ "\"" ++ rest
+
 
 -- | Updates a state according to a command.
 -- Performs file IO via ioChan if needed.
@@ -68,6 +118,50 @@ renderStatements _ = error "Not implemented 5"
 -- State update must be executed atomically (STM).
 -- Right contains an optional message to print, updated state
 -- is stored in transactinal variable
-stateTransition :: TVar Lib2.State -> Command -> Chan StorageOp ->
-                   IO (Either String (Maybe String))
-stateTransition _ _ ioChan = return $ Left "Not implemented 6"
+stateTransition :: TVar Lib2.State -> Command -> Chan StorageOp -> IO (Either String (Maybe String, String))
+stateTransition stateVar command ioChan =
+    case command of
+        LoadCommand -> do
+            responseChan <- newChan
+            writeChan ioChan (Load responseChan)
+            content <- readChan responseChan
+            case parseStatements content of
+                Left err -> return $ Left err
+                Right (Batch queries, _) -> atomically $ do
+                    writeTVar stateVar Lib2.emptyState
+                    executeBatch stateVar Lib2.emptyState queries
+                Right _ -> return $ Left "Unexpected non-batch command in loaded content."
+        
+        SaveCommand -> do
+            currentState <- readTVarIO stateVar
+            let stateStr = renderStatements (marshallState currentState)
+            responseChan <- newChan
+            writeChan ioChan (Save stateStr responseChan)
+            readChan responseChan
+            return $ Right (Just "State saved", show currentState)
+        
+        StatementCommand statements ->
+            atomically $ executeStatements stateVar statements
+
+executeStatements :: TVar Lib2.State -> Statements -> STM (Either String (Maybe String, String))
+executeStatements stateVar statements = do
+    currentState <- readTVar stateVar
+    case statements of
+        Single query ->
+            case Lib2.stateTransition currentState query of
+                Left err -> return $ Left err
+                Right (msg, newState) -> do
+                    writeTVar stateVar newState
+                    return $ Right (msg, show newState)
+        Batch queries ->
+            executeBatch stateVar currentState queries
+
+
+executeBatch :: TVar Lib2.State -> Lib2.State -> [Lib2.Query] -> STM (Either String (Maybe String, String))
+executeBatch stateVar initialState [] = do
+    writeTVar stateVar initialState
+    return $ Right (Nothing, show initialState)
+executeBatch stateVar initialState (q:qs) =
+    case Lib2.stateTransition initialState q of
+        Left err -> return $ Left err
+        Right (_, newState) -> executeBatch stateVar newState qs
