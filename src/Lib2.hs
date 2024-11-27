32c17b30f1d3f9ac31ec36aa6a7156a39baca593
Darijus Daužvardis <dauzva@gmail.com>
2024-11-27 02:20:01 +0000

    added lib2

--- src/Lib2.hs
+++ src/Lib2.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE InstanceSigs #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Redundant bracket" #-}
 module Lib2
     ( Query(..),
     parseQuery,
@@ -6,34 +8,217 @@ module Lib2
     emptyState,
     stateTransition
     ) where
+import qualified Data.Char as C
+import qualified Data.List as L
+
+
+validGenres :: [String]
+validGenres = [
+    "Fiction",
+    "Non-Fiction",
+    "Mystery",
+    "Thriller",
+    "Romance",
+    "Sci-Fi",
+    "Fantasy",
+    "Biography"
+    ]
 
 -- | An entity which represets user input.
 -- It should match the grammar from Laboratory work #1.
 -- Currently it has no constructors but you can introduce
 -- as many as needed.
-data Query
+data Query = 
+    AddQuery String |
+    RemoveQuery String |
+    ListQuery
 
 -- | The instances are needed basically for tests
 instance Eq Query where
-  (==) _ _= False
+    (==) ListQuery ListQuery = True
+    (==) (AddQuery a) (AddQuery b) = a == b
+    (==) (RemoveQuery a) (RemoveQuery b) = a == b
+    (==) _ _ = False
 
 instance Show Query where
-  show _ = ""
+    show (AddQuery a)       = show a
+    show (RemoveQuery a)    = show a
+    show ListQuery          = show "List:"
 
 -- | Parses user's input.
 -- The function must have tests.
 parseQuery :: String -> Either String Query
-parseQuery _ = Left "Not implemented 2"
+parseQuery str = 
+    case parseCmd str of
+        Left(eCmd) -> Left(eCmd)
+        Right(rCmd) -> 
+			case parseBook rCmd of
+				Left(eBook) -> Left(eBook)
+				Right(rBook) -> Right(rBook)
+
+
+parseComma :: String -> Either String String
+parseComma str =
+	let
+		rest1 = L.takeWhile C.isSpace str
+		rest2 = drop (length rest1) str
+		comma = L.take 1 rest2
+		rest3 = drop 1 rest2
+		rest4 = L.takeWhile C.isSpace rest3
+		rest = drop (length rest4) rest3
+	in
+		if comma == "," then Right rest
+		else Left "Invalid separator"
+
+parseCmd :: String -> Either String Query
+parseCmd str =
+        let
+                cmd = L.takeWhile C.isAlpha str
+                rest1 = drop (length cmd) str
+                rest2 = L.takeWhile C.isSpace rest1
+                rest = drop (length rest2) rest1
+        in
+                if      cmd == "add"    then Right (AddQuery rest)
+                else if cmd == "remove" then Right (RemoveQuery rest)
+                else if cmd == "list"   then Right (ListQuery)
+                else                    Left "Invalid command"
+
+parseBook :: Query -> Either String Query
+parseBook ListQuery = Right ListQuery
+parseBook (AddQuery str) = 
+	case parseTitle str of
+		Left(e1) -> Left(e1)
+		Right(title, r1) ->
+			case parseAuthor r1 of
+				Left(e2) -> Left(e2)
+				Right(author, r2) -> 
+					case parseGenre r2 of
+						Left(e3) -> Left(e3)
+						Right(genre, r3) -> 
+							case parseYear r3 of
+								Left(e4) -> Left(e4)
+								Right(year, r4) -> 
+									case parsePrice r4 of
+										Left(e5) -> Left(e5)
+										Right(price, r5) -> Right(AddQuery $ title++", "++author++", "++genre++", "++year++", "++price)
+parseBook (RemoveQuery str) =
+	case parseId str of
+		Left(e1) -> Left(e1)
+		Right(r1) -> Right(RemoveQuery r1)
+
+parseTitle :: String -> Either String (String, String)
+parseTitle str = parseTitle' str []
+
+parseTitle' :: String -> String -> Either String (String, String)
+parseTitle' str [] = 
+	let
+		titleStart = L.take 1 str
+		rest1 = drop 1 str
+		title = L.takeWhile (/= '\"') rest1
+		rest2 = drop (length title) rest1
+		rest3 = L.takeWhile C.isSpace rest2
+		rest = drop (length rest3) rest2
+		titleEnd = L.take 1 rest
+	in
+		if (titleStart == "\"" && titleEnd == "\"") then parseTitle' rest title
+		else Left "Invalid Title syntax"
+parseTitle' str acc = 
+	let
+		titleStart = L.take 1 str
+		rest1 = drop 1 str
+		title = L.takeWhile (/= '\"') rest1
+		rest2 = drop (length title) rest1
+		rest3 = L.takeWhile C.isSpace rest2
+		rest = drop (length rest3) rest2
+		titleEnd = L.take 1 rest
+	in
+		if (titleStart == "\"" && titleEnd == "\"") then parseTitle' rest (acc++"\""++title)
+		else Right (acc, title)
+
+parseAuthor :: String -> Either String (String, String)
+parseAuthor str =
+	case parseComma str of
+		Left(e1) -> Left(e1)
+		Right(r1) -> 
+			let
+				name = L.takeWhile C.isAlpha r1
+				rest1 = drop (length name) r1
+				rest2 = L.takeWhile C.isSpace rest1
+				rest3 = drop (length rest2) rest1
+				surname = L.takeWhile C.isAlpha rest3
+				rest = drop (length surname) rest3
+			in
+				if (name /= [] && surname /= []) then Right(name++" "++surname, rest)
+				else Left "Invalid Author"
+
+parseGenre :: String -> Either String (String, String)
+parseGenre str = 
+	case parseComma str of
+		Left(e1) -> Left(e1)
+		Right(r1) ->
+			let
+				genre = L.takeWhile (\c -> C.isAlpha c || c == '-') r1
+				rest = drop (length genre) r1
+			in
+				if genre `elem` validGenres then Right (genre, rest)
+				else Left "Invalid Genre"
+					
+parseYear :: String -> Either String (String, String)
+parseYear str =
+	case parseComma str of
+		Left(e1) -> Left(e1)
+		Right(r1) -> 
+			let
+				year = L.take 4 r1
+				rest = drop (length year) r1
+			in
+				if all C.isDigit year then Right(year, rest)
+				else Left "Invalid Year"
+
+parsePrice :: String -> Either String (String, String)
+parsePrice str =
+	case parseComma str of
+		Left(e1) -> Left(e1)
+		Right(r1) -> 
+			let
+				price = L.takeWhile C.isNumber r1
+				r2 = drop (length price) r1
+				decimal = L.take 1 r2
+				r3 = drop 1 r2
+				price1 = L.takeWhile C.isNumber r3
+				rest = drop (length price1) r3
+			in
+				if (price /= [] && (null decimal || all C.isSpace decimal) && (null rest || all C.isSpace rest)) then Right(price, rest)
+				else if (price /= [] && decimal == "." && price1 /= [] && (length price1 == 1 || length price1 == 2) && (null rest || all C.isSpace rest)) then Right(price++"."++price1, rest)
+				else Left "Invalid Price"
+
+parseId :: String -> Either String String
+parseId str =
+	let
+        r1 = L.takeWhile C.isSpace str
+        r2 = drop (length r1) str
+        id = L.takeWhile C.isNumber r2
+	in
+        if id /= [] then Right id
+        else Left "Invalid ID"
+
 
 -- | An entity which represents your program's state.
 -- Currently it has no constructors but you can introduce
 -- as many as needed.
-data State
+data State = State [Query]
+instance Show State where
+    show (State queries) = 
+        let formatQuery (AddQuery a) = a
+            formatQuery (RemoveQuery a) = a
+            formatQuery ListQuery = "List:"
+        in unlines (map formatQuery queries)
+
 
 -- | Creates an initial program's state.
 -- It is called once when the program starts.
 emptyState :: State
-emptyState = error "Not implemented 1"
+emptyState = State []
 
 -- | Updates a state according to a query.
 -- This allows your program to share the state
@@ -41,4 +226,29 @@ emptyState = error "Not implemented 1"
 -- Right contains an optional message to print and
 -- an updated program's state.
 stateTransition :: State -> Query -> Either String (Maybe String, State)
-stateTransition _ _ = Left "Not implemented 3"
+stateTransition (State old) q = 
+    case q of
+        ListQuery ->
+            Right (Just (show (State old)), State old)
+        AddQuery a ->
+            let
+                id = AddQuery ((show (length old + 1)) ++ ". " ++ a)
+                new = State (old ++ [id])  
+            in
+                Right (Just (show new), new)
+        RemoveQuery idStr ->
+            let
+                id = read idStr :: Int
+                getId (AddQuery str) = takeWhile (/= '.') str
+                matchId x = getId x == idStr
+                newList = L.deleteBy (\x y -> matchId x && matchId y) (AddQuery idStr) old
+                reindexed = zipWith reindex [1..] newList
+                reindex n (AddQuery str) = 
+                    let content = dropWhile (/= '.') str
+                    in AddQuery (show n ++ content)
+                new = State reindexed
+            in
+				if ((id > (length old)) || (id < 1)) then Left "Id not found"
+                else Right (Just (show new), new)
+		
+
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,33 +1,116 @@
 {-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+
 import Test.Tasty ( TestTree, defaultMain, testGroup )
 import Test.Tasty.HUnit ( testCase, (@?=) )
 import Test.Tasty.QuickCheck as QC
-
-import Data.List
-import Data.Ord
-
+import Control.Concurrent ( Chan, newChan, writeChan, readChan )
+import Control.Monad.IO.Class ( liftIO )
 import Lib1 qualified
 import Lib2 qualified
+import Lib3 qualified
+import GHC.Generics (Generic)
+import Control.Concurrent.STM (TVar, newTVarIO, readTVarIO, atomically)
+import Test.QuickCheck.Monadic (monadicIO, assert)
 
+-- Main test entry point
 main :: IO ()
 main = defaultMain tests
 
+-- Test tree that includes unit tests and property tests
 tests :: TestTree
 tests = testGroup "Tests" [unitTests, propertyTests]
 
+-- Unit tests for functionality
 unitTests :: TestTree
-unitTests = testGroup "Lib1 tests"
-  [ testCase "List of completions is not empty" $
-      null Lib1.completions @?= False,
-    testCase "Parsing case 1 - give a better name" $
-      Lib2.parseQuery "" @?= (Left "Some error message"),
-    testCase "Parsing case 2 - give a better name" $
-      Lib2.parseQuery "o" @?= (Left "Some error message")
+unitTests = testGroup "Book Store Tests" [
+    testCase "Empty input" $
+        Lib2.parseQuery "" @?= Left "Invalid command",
+    
+    testCase "Invalid command" $
+        Lib2.parseQuery "hello" @?= Left "Invalid command",
+    
+    testCase "Add book - valid input" $
+        Lib2.parseQuery "add \"The Hobbit\", John Tolkien, Fantasy, 1937, 29.99" @?= 
+        Right (Lib2.AddQuery "The Hobbit, John Tolkien, Fantasy, 1937, 29.99"),
+
+    testCase "Add book - invalid title" $
+        Lib2.parseQuery "add The Hobbit, John Tolkien, jasdja, 1937, 29.99" @?= 
+        Left "Invalid Title syntax",
+
+    testCase "Add book - invalid author" $   
+        Lib2.parseQuery "add \"The Hobbit\", John 123, jasdja, 1937, 29.99" @?= 
+        Left "Invalid Author",
+    
+    testCase "Add book - invalid year" $
+        Lib2.parseQuery "add \"The Hobbit\", John Tolkien, Fantasy, 1, 29.99" @?= 
+        Left "Invalid Year",
+    
+    testCase "Add book - invalid genre" $
+        Lib2.parseQuery "add \"The Hobbit\", John Tolkien, jasdja, 1937, 29.99" @?= 
+        Left "Invalid Genre",
+    
+    testCase "Add book - invalid price format" $
+        Lib2.parseQuery "add \"The Hobbit\", John Tolkien, Fantasy, 1937, 29.999" @?= 
+        Left "Invalid Price",
+    
+    testCase "Remove - valid id" $
+        Lib2.parseQuery "remove 1" @?= Right (Lib2.RemoveQuery "1"),
+    
+    testCase "Remove - invalid id format" $
+        Lib2.parseQuery "remove abc" @?= Left "Invalid ID",
+    
+    testCase "List command" $
+        Lib2.parseQuery "list" @?= Right Lib2.ListQuery
+    ]
+
+-- Property-based tests for Lib3
+propertyTests :: TestTree
+propertyTests = testGroup "Lib3 Property Tests"
+  [
+    QC.testProperty "parse and render round-trip for Statements" $
+      \statements -> 
+        let rendered = Lib3.renderStatements statements
+            parsed = Lib3.parseStatements rendered
+        in case parsed of
+            Right (s, "") -> s == statements
+            _ -> False,
+
+    QC.testProperty "storage operations round-trip" $
+      \content -> ioRoundTrip content
   ]
 
-propertyTests :: TestTree
-propertyTests = testGroup "some meaningful name"
-  [
-    QC.testProperty "sort == sort . reverse" $
-      \list -> sort (list :: [Int]) == sort (reverse list)
-  ]
+instance QC.Arbitrary Lib3.StorageOp where
+  arbitrary = QC.oneof [ Lib3.Save <$> arbitrary <*> arbitrary
+                       , Lib3.Load <$> arbitrary ]
+
+instance QC.Arbitrary Lib3.Statements where
+  arbitrary = QC.oneof [ Lib3.Single <$> arbitrary
+                       , Lib3.Batch <$> arbitrary ]
+
+instance QC.Arbitrary Lib3.Command where
+  arbitrary = QC.oneof [ return Lib3.LoadCommand
+                       , return Lib3.SaveCommand
+                       , Lib3.StatementCommand <$> arbitrary ]
+
+instance QC.Arbitrary Lib2.Query where
+  arbitrary = QC.oneof [
+      Lib2.AddQuery <$> arbitrary
+    , Lib2.RemoveQuery <$> arbitrary
+    , return Lib2.ListQuery
+    ]
+
+-- Property test for storage operations (load and save)
+ioRoundTrip :: String -> Property
+ioRoundTrip content = monadicIO $ do
+  ioChan <- liftIO newChan
+  responseChan <- liftIO newChan
+  liftIO $ writeChan ioChan (Lib3.Save content responseChan)
+  savedContent <- liftIO $ readChan responseChan
+  liftIO $ writeChan ioChan (Lib3.Load responseChan)
+  loadedContent <- liftIO $ readChan responseChan
+  assert (savedContent == loadedContent)
+
+
