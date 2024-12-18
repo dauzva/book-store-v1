b4fdfce907b8e264ed3e568b8ad5c7929c6c143d
Darijus Daužvardis <dauzva@gmail.com>
2024-12-18 09:51:29 +0000

    fix

--- app4-client/Main.hs
+++ app4-client/Main.hs
@@ -1,12 +1,26 @@
 module Main (main) where
 
-import Data.ByteString
+import Data.ByteString (ByteString)
 import Network.Wreq
 import Data.String.Conversions
 import Control.Lens
+import System.IO (putStr, hFlush, stdout)
 
 main :: IO ()
 main = do
-    let rawRequest = cs "works?" :: ByteString
-    resp <- post "http://localhost:3000" rawRequest
-    putStrLn $ cs $ resp ^. responseBody
+    putStrLn "Book Store Client"
+    putStrLn "Enter command (or 'quit' to exit):"
+    mainLoop
+
+mainLoop :: IO ()
+mainLoop = do
+    putStr "> "
+    hFlush stdout
+    cmd <- getLine
+    if cmd == "quit"
+    then putStrLn "Exiting client..."
+    else do
+        let rawRequest = cs cmd :: ByteString
+        resp <- post "http://localhost:3000" rawRequest
+        putStrLn $ cs $ resp ^. responseBody
+        mainLoop
